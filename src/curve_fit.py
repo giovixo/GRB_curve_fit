3b83f75eeaa3cac839803fb8c25e028b483928f0
Giovanni De Cesare <decesare.giovanni@gmail.com>
2018-02-23 14:02:58 +0100

    very first curve_fit main program

---
+++ src/curve_fit.py
@@ -0,0 +1,13 @@
+def main():
+    '''
+    GRB curve fit: fantastic code that fits thousands of data
+    '''
+    print "---------------------------------------------------------"
+    print "GRB curve fit: fantastic code that fits thousands of data"
+    print "---------------------------------------------------------"
+    # Import the gfit module
+    import gfit
+
+
+if __name__ == "__main__":
+    main()
